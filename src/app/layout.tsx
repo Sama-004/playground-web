7ddd2bff1bbf9bfe2ffac3b3ae525cd88f89dfd9
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-10-03 18:46:48 +0530

    test prettier workflow again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,27 @@
 // types
-import { ReactNode } from 'react';
-import type { Metadata } from 'next';
+import { ReactNode } from "react";
+import type { Metadata } from "next";
 
 // styles
 import '@/styles/globals.css';
 
 export const metadata: Metadata = {
-  title: 'DiceDB Playground',
-  description:
-    'DiceDB is an in-memory, real-time, and reactive database with Redis and SQL support optimized for modern hardware and building real-time applications.', // Added meaningful description
-  icons: {
-    icon: 'https://dicedb.io/icon.png',
-  },
+	title: 'DiceDB Playground',
+	description:
+		'DiceDB is an in-memory, real-time, and reactive database with Redis and SQL support optimized for modern hardware and building real-time applications.', // Added meaningful description
+	icons: {
+		icon: 'https://dicedb.io/icon.png',
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: ReactNode;
+	children: ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
 }
